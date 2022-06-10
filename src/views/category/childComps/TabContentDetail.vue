c61f692962cc28dc3be1c6651319891923900ecc
tigeedev <970853117@qq.com>
2022-06-10 20:04:32 +0800

    完善项目

---
+++ src/views/category/childComps/TabContentDetail.vue
@@ -0,0 +1,33 @@
+<template>
+  <grid-view  :lineSpace="15" :v-margin="20" v-if="categoryDetail">
+    <goods-list-item v-for="(item, index) in categoryDetail" :key="index" :product="item"></goods-list-item>
+  </grid-view>
+</template>
+
+<script>
+  import GridView from 'components/common/grid/GridView'
+  import GoodListItem from 'components/content/goods/GoodListItem'
+
+  export default {
+    name: "TabContentDetail",
+    components: {
+      GridView,
+      GoodListItem
+    },
+    props: {
+      categoryDetail: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    created(){
+      console.log(this.categoryDetail)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
